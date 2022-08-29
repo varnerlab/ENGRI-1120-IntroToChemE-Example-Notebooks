29ab3f44f89d22436d4cca8abb5aed65f2668704
Sandra-Vadhin <48500531+Sandra-Vadhin@users.noreply.github.com>
2022-08-29 12:54:45 -0400

    updating simulate_total_mass_balances function
    had mismatch between input argument (α) and variable used (σ)

--- labs/lab-1-fun-w-mass-balances/ENGRI-1120-Lab-1-CodeLib.jl
+++ labs/lab-1-fun-w-mass-balances/ENGRI-1120-Lab-1-CodeLib.jl
@@ -80,7 +80,6 @@ function simulate_total_mass_balances(config::Dict{String,Any}; α::Float64 = 0.
         ṁ₃ = κ * X[t, 1]
 
         # update the state array -
-        X[t+1, 1] = X[t, 1] + h * (ṁ₁ + ṁ₂ - ṁ₃) + σ * sqrt(h) * rand(Z)
     end
 
     # return -
