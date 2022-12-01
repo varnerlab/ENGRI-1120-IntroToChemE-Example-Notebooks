9383554cae6d6df29d3d567b000440e0f47dd0c0
Jeffrey Varner <jdv27@cornell.edu>
2022-12-01 12:31:49 -0500

    Working on templates
    project templates

---
+++ project/ENGRI-1120-Project-CodeLib.jl
@@ -0,0 +1,7 @@
+# my codes -
+include(joinpath("src","Types.jl"))
+include(joinpath("src","Stoichiometric.jl"))
+include(joinpath("src", "Parser.jl"))
+include(joinpath("src","Flux.jl"))
+include(joinpath("src","MSU.jl"))
+include(joinpath("src", "Units.jl"))
